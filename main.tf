c1d301107eea473f758f812bd197bd8ea747f9d8
shinjikanai <shinjikanai@users.noreply.github.com>
2017-11-05 11:47:22 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,23 @@
-provider "softlayer" {
-    username = "${var.sl_username}"
-    api_key = "${var.sl_api_key}"
+provider "ibm" {
+  bluemix_api_key = "${var.bxapikey}"
+  softlayer_username = "${var.sl_username}"
+  softlayer_api_key = "${var.sl_api_key}"
 }
 
-data "softlayer_ssh_key" "public_key" {
-  label = "${var.key_name}"
-}
 
 #A way to get image id by name programatically (shinji).
 #data "ibm_compute_image_template" "img_tpl" {
 #    name = "ICP21_installer(allinone)"
 #}
 
-resource "softlayer_virtual_guest" "icpmaster" {
+resource "ibm_compute_vm_instance" "icpmaster" {
     count       = "${var.master["nodes"]}"
 
     datacenter  = "${var.datacenter}"
     domain      = "${var.domain}"
-    hostname    = "${format("${lower(var.instance_name)}-master%01d", count.index + 1) }"
-
+    #hostname    = "${format("${lower(var.instance_name)}-master%01d", count.index + 1) }"
+    hostname          = "${var.instance_name}"
+    
     #os_reference_code = "UBUNTU_16_64"
     image_id          = "${var.image_id}"
     cores       = "${var.master["cpu_cores"]}"
@@ -28,10 +27,12 @@ resource "softlayer_virtual_guest" "icpmaster" {
     network_speed         = "${var.master["network_speed"]}"
     hourly_billing        = "${var.master["hourly_billing"]}"
     private_network_only  = "${var.master["private_network_only"]}"
-
+    #post_install_script_uri = "${var.post_install_script_uri}"
     #user_metadata = "{\"value\":\"newvalue\"}"
 
     #ssh_key_ids = ["${data.softlayer_ssh_key.public_key.id}"]
 }
+
+
 
 
